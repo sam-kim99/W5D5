464c6597e0acfd82e0a7f0eaa76936b29b33e231
sam-kim99 <samkim0451@gmail.com>
2024-02-09 14:31:00 -0500

    complete first 3 queries

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
-
+  Movie.where(score: 2..3).select(:id, :title, :score)
 end
 
 def harrison_ford
@@ -20,7 +20,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
-
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where(actors: { name: 'Harrison Ford' })
+    .where.not(castings: { ord: 1 })
 end
 
 def biggest_cast
@@ -37,7 +41,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
-  
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .group('movies.id')
+    .order('COUNT(actors.id) DESC')
+    .limit(3)
 end
 
 def directed_by_one_of(them)
